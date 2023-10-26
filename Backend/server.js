f5ee37903036f35aa07560d9fb426b033afdeb68
NaN-Didital <nan-didital@192.168.50.77>
2023-10-26 17:38:34 +0000

    dashb

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
 
   if (req.url === "/auth/register" && req.method === "POST") {
     let body = "";
-    req.on("data", (data) => {     
+    req.on("data", (data) => {
       body += data;
     });
     req.on("end", () => {
@@ -27,12 +27,11 @@ const server = http.createServer((req, res) => {
       console.log(body);
       if (verif) {
         res.end("Désolé ce utilisateur exist déjà .");
-      }
-      else{
-        let oldId = userData[userData.length-1]
-        let id = oldId === undefined ? 1: parseInt(oldId.id)+1
+      } else {
+        let oldId = userData[userData.length - 1];
+        let id = oldId === undefined ? 1 : parseInt(oldId.id) + 1;
         console.log(id);
-        let data = {id:id, ...body};
+        let data = { id: id, ...body };
         console.log(data);
         userData.push(data);
         fs.writeFileSync(
@@ -40,18 +39,16 @@ const server = http.createServer((req, res) => {
           JSON.stringify(userData),
           { encoding: "UTF-8" }
         );
-      res.end("Inscription effectué avec succes !!!");
+        res.end("Inscription effectué avec succes !!!");
       }
-      
     });
-  }
-  else if(req.url === "/auth/login" && req.method === "POST"){
+  } else if (req.url === "/auth/login" && req.method === "POST") {
     let body = "";
-    req.on("data", (data)=>{
+    req.on("data", (data) => {
       body += data;
       console.log(body);
-    })
-    req.on("end", () =>{
+    });
+    req.on("end", () => {
       body = JSON.parse(body);
       console.log(body);
 
@@ -59,25 +56,55 @@ const server = http.createServer((req, res) => {
         path.join("assets", "Data", "users.json"),
         { encoding: "UTF-8" }
       );
-        userData = JSON.parse(userData)
-      let verify = userData.find((items) => items.email === body.email && items.password === body.password)
+      userData = JSON.parse(userData);
+      let verify = userData.find(
+        (items) =>
+          items.email === body.email && items.password === body.password
+      );
       if (verify) {
-        const {email,name} = verify;
-        console.log(email,name);
-        let data = {status:true, email, name,id:verify.id, message:"Bienvenue vous êtes maintenant connecté"}
-        res.end(JSON.stringify(data));  
-      }
-      else{
-        let data = {status:false,message:"Email ou mot de passe incorrecte"}
+        const { email, name } = verify;
+        console.log(email, name);
+        let data = {
+          status: true,
+          email,
+          name,
+          id: verify.id,
+          message: "Bienvenue vous êtes maintenant connecté",
+        };
+        res.end(JSON.stringify(data));
+      } else {
+        let data = {
+          status: false,
+          message: "Email ou mot de passe incorrecte",
+        };
         res.end(JSON.stringify(data));
       }
     });
+  } else if (req.url === "/addTasks" && req.method === "POST") {
+    let body = "";
+    req.on("data", (data) => {
+      body += data;
+      console.log(body);
+    });
+    req.on("end", () => {
+      body = JSON.parse(body);
+      console.log(body);
+      let taks = fs.readFileSync(path.join("assets", "Data", "tasks.json"), {
+        encoding: "UTF-8",
+      });
+      console.log(taks);
 
-  };
-  // else if (){
-
-  // }
-}); 
+      taks = JSON.parse(taks);
+      taks.push(body);
+      fs.writeFileSync(
+        path.join("assets", "Data", "users.json"),
+        JSON.stringify(taks),
+        { encoding: "UTF-8" }
+      );
+      res.end("Tache bien enrégistré !");
+    });
+  }
+});
 
 server.listen(4000, () => {
   console.log("server running");
---
+++ todo/js/dashboard.js
@@ -0,0 +1,4 @@
+window.addEventListener("DOMContentLoaded", ()=>{
+    let input = document.querySelector("input");
+    let bouton = document.querySelector("button");
+})
